04e8981258f0f1b6dfb94262a481aefc20c870fe
cagataycali <calicagatay@icloud.com>
2016-10-22 06:41:06 -0700

    ✔️  Comment added

--- examples/uploadFile.js
+++ examples/uploadFile.js
@@ -5,7 +5,7 @@ var nightmare = Nightmare({ show: true});
 nightmare
   .goto('https://www.tutorialspoint.com/php/src/upload.php')
   .upload('[type="file"]', 'assets/tombili.jpg')
-  .wait(5000)
+  .wait(5000) /* If you dont want to submit form you can remove these line :) */
   .click('[type="submit"]')
   .then(function (result) {
     console.log(result)
